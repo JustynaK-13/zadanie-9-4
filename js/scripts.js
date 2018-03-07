30f59b404f14c1b457780d0f9c44b9a1c5e42edd
Justyna Kaczorowska <js.kaczorowska@gmail.com>
2018-03-07 10:52:48 +0100

    Change content

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
-function drawTree() {
-  
-  for (var i = 0 ; i < 5 ; i++) {
-  var star = ''; 
-    for (var j= 0 ; j <= i ; j++) {
-       
-    }
-  }
-
+function drawTree (i  = 5) {
+  for ( var i = 0 ; i < 5 ; i++) {
+  var star = ""; 
+     for ( var j = 0 ; j <= i ; j++) {
+     star += "*";
+     }
+    console.log(star);
+  }  
 }
